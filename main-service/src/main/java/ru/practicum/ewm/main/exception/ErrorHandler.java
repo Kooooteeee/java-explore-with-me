c2f87004141a3cd80b77844ba3a13affda81913b
Maksim Kalachev <maxiim.kalachik@gmail.com>
2026-04-23 22:59:57 +0300

    фиксы: Забыл проставить статусы в хендлере, это конечно мдааа

--- main-service/src/main/java/ru/practicum/ewm/main/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/ewm/main/exception/ErrorHandler.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 @RestControllerAdvice
 public class ErrorHandler {
 
@@ -16,6 +19,7 @@ public class ErrorHandler {
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleNotFoundException(final NotFoundException e) {
         return new ApiError(
                 HttpStatus.NOT_FOUND.toString(),
@@ -26,6 +30,7 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleConflictException(final ConflictException e) {
         return new ApiError(
                 HttpStatus.CONFLICT.toString(),
@@ -36,6 +41,7 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         return new ApiError(
                 HttpStatus.BAD_REQUEST.toString(),
@@ -46,6 +52,7 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleConstraintViolationException(final ConstraintViolationException e) {
         return new ApiError(
                 HttpStatus.BAD_REQUEST.toString(),
@@ -56,10 +63,22 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError handleThrowable(final Throwable e) {
         return new ApiError(
                 HttpStatus.INTERNAL_SERVER_ERROR.toString(),
                 "Internal server error.",
+                e.getMessage(),
+                LocalDateTime.now().format(FORMATTER)
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        return new ApiError(
+                HttpStatus.BAD_REQUEST.toString(),
+                "Incorrectly made request.",
                 e.getMessage(),
                 LocalDateTime.now().format(FORMATTER)
         );
