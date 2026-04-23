c8858be7947df8e32731e5ef5581e76b5a30b7f7
Maksim Kalachev <maxiim.kalachik@gmail.com>
2026-04-23 22:49:06 +0300

    поееехали... фиксы: CheckStyle с лишним импортом и убрал пустой класс

--- main-service/src/main/java/ru/practicum/ewm/main/event/EventDto.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.ewm.main.event;
-
-public class EventDto {
-}
--- main-service/src/main/java/ru/practicum/ewm/main/event/EventRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/main/event/EventRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.practicum.ewm.main.request.RequestStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
